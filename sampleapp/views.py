bc28328d79c465b78eedf701814d212b17f7d042
TanaPatee <chaitana3162@gmail.com>
2023-01-16 11:36:46 +0700

    update 11:36 16/01/23

--- sampleapp/views.py
+++ sampleapp/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
-
+def Tana(requset):
+    name = "Tana"
+    Age = 21
+    Blance = 10000000.00
+    context = {'name':name, 'Age':Age, 'Balance':Blance }
 
 # Create your views here.

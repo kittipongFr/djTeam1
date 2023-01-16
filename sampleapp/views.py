d58be49231bbd4f6f2b8b3cdf5cb7572fdfef300
kittipongFr <kittipong.pa@rmuti.ac.th>
2023-01-16 11:50:16 +0700

    First commit by frank 16/1/23 11.50

--- sampleapp/views.py
+++ sampleapp/views.py
@@ -6,3 +6,12 @@ def Tana(requset):
     context = {'name':name, 'Age':Age, 'Balance':Blance }
 
 # Create your views here.
+
+def home(request):
+    nameteam = 'Team Frank'
+    rank = 1
+    company = "DBT 3R1"
+
+    return render(request, '1.html', {
+        'nameteam': nameteam,'rank' : rank , 'company' : company
+    })

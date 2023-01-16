8a3e97b8e9e92f76c13aaaff454937249e099dbd
TanaPatee <chaitana3162@gmail.com>
2023-01-16 11:22:41 +0700

    update 11:22 16/01/23

--- djTeam1/urls.py
+++ djTeam1/urls.py
@@ -19,6 +19,5 @@ from sampleapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name = 'home') ,
-    path('sampleapp/', include('sampleapp.urls'))
+
 ]
--- sampleapp/views.py
+++ sampleapp/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
-def home(requset):
-    name ='TeamFrank'
-    rank = 1
-    balance = 10000000.00
+
 
 # Create your views here.
